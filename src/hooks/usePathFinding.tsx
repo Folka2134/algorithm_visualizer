c26719de05d0b308ba629735ad816d4665c34f45
folka <folka2134@gmail.com>
2024-06-28 15:00:36 +0100

    created pathfinding context

---
+++ src/hooks/usePathFinding.tsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import {
+  PathfindingContext,
+  PathfindingContextInterface,
+} from "../context/PathfindingContext";
+
+export const usePathfinding = (): PathfindingContextInterface => {
+  const context = useContext(PathfindingContext);
+
+  if (!context) {
+    throw new Error(
+      "usePathfinding must be used within the PathfindingProvider"
+    );
+  }
+
+  return context;
+};
