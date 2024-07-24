be6771fca21e19e96c7ffe5c938abeff6bb89869
folka2134 <folka2134@gmail.com>
2024-07-24 19:06:05 +0100

    created mazeSelectType

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,20 +1,23 @@
-export type AlgorithmType = "DIJKSTRA" | "A_STAR" | "BFS" | "DFS"
+export type AlgorithmType = "DIJKSTRA" | "A_STAR" | "BFS" | "DFS";
 
-export type MazeType = "NONE" | "BINARY_TREE" | "RECURSIVE_DIVISION"
-
-export type TileType = {
-  row: number
-  col: number
-  isStart: boolean
-  isEnd: boolean
-  isWall: boolean
-  isPath: boolean
-  distance: number
-  isTraversed: boolean
-  parent: TileType | null
+export type MazeType = "NONE" | "BINARY_TREE" | "RECURSIVE_DIVISION";
+export interface MazeSelectType {
+  name: string;
+  value: MazeType;
 }
 
-export type GridType = TileType[][]
+export type TileType = {
+  row: number;
+  col: number;
+  isStart: boolean;
+  isEnd: boolean;
+  isWall: boolean;
+  isPath: boolean;
+  distance: number;
+  isTraversed: boolean;
+  parent: TileType | null;
+};
 
-export type SpeedType = 2 | 1 | 0.5
+export type GridType = TileType[][];
 
+export type SpeedType = 2 | 1 | 0.5;
