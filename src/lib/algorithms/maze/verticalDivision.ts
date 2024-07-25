48730af966b7bede07040e61ab12ebd9ccf73953
folka2134 <folka2134@gmail.com>
2024-07-25 16:47:58 +0100

    added comments

--- src/lib/algorithms/maze/verticalDivision.ts
+++ src/lib/algorithms/maze/verticalDivision.ts
@@ -1,9 +1,9 @@
 import { SPEEDS, WALL_TILE_STYLE } from "../../../utils/constants";
 import { getRandInt, isEqual, sleep } from "../../../utils/helpers";
 import { GridType, SpeedType, TileType } from "../../../utils/types";
-import { recursiveDivision } from "./recursiveDivision";
+import recursiveDivision from "./recursiveDivision";
 
-export const verticalDivision = async ({
+export async function verticalDivision({
   grid,
   startTile,
   endTile,
@@ -23,24 +23,27 @@ export const verticalDivision = async ({
   width: number;
   setIsDisabled: (isDisabled: boolean) => void;
   speed: SpeedType;
-}) => {
-  const makeWallAt = col + getRandInt(0, width - 1) * 2 + 1;
-  const makePassageAt = row + getRandInt(0, height) * 2;
+}) {
+  const makeWallAt = col + getRandInt(0, width - 1) * 2 + 1; // Determine the column to place the wall
+  const makePassageAt = row + getRandInt(0, height) * 2; // Determine the row to leave a passage
 
-  for (let i = 0; i < 2 * height - 1; i++) {
-    if (makePassageAt != row + i) {
+  for (let i = 0; i < 2 * height - 1; i += 1) {
+    // Create the vertical wall
+    if (makePassageAt !== row + i) {
       if (
-        !isEqual(grid[row + i][makeWallAt], startTile) &&
-        !isEqual(grid[row + i][makeWallAt], endTile)
+        !isEqual(grid[row + i][makeWallAt], startTile) && // Check if the current tile is not the start tile
+        !isEqual(grid[row + i][makeWallAt], endTile) // Check if the current tile is not the end tile
       ) {
-        grid[row + 1][makeWallAt].isWall = true;
+        grid[row + i][makeWallAt].isWall = true; // Set the current tile as a wall
 
         document.getElementById(`${row + i}-${makeWallAt}`)!.className =
-          `${WALL_TILE_STYLE} animate-wall`;
-        await sleep(10 * SPEEDS.find((s) => s.value === speed)!.value - 5);
+          `${WALL_TILE_STYLE} animate-wall`; // Add wall style and animation
+        await sleep(10 * SPEEDS.find((s) => s.value === speed)!.value - 5); // Wait for animation
       }
     }
   }
+
+  // Recursively divide the sections to the left and right of the wall
   await recursiveDivision({
     grid,
     startTile,
@@ -63,4 +66,4 @@ export const verticalDivision = async ({
     setIsDisabled,
     speed,
   });
-};
+}
