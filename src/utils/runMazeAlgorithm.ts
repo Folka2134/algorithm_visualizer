2bbbd40aa25453d80d6325a2873ff4f6e06657dc
folka2134 <folka2134@gmail.com>
2024-07-25 16:31:47 +0100

    added recursive division algorithm

--- src/utils/runMazeAlgorithm.ts
+++ src/utils/runMazeAlgorithm.ts
@@ -1,4 +1,7 @@
 import { binaryTree } from "../lib/algorithms/maze/binaryTree";
+import { recursiveDivision } from "../lib/algorithms/maze/recursiveDivision";
+import { MAX_COLS, MAX_ROWS, SPEEDS } from "./constants";
+import { constructBoarder } from "./constructBoarder";
 import { GridType, MazeType, SpeedType, TileType } from "./types";
 
 // type runMazeALgorithmProps = {
@@ -20,5 +23,22 @@ export const runMazeALgorithm = async (
 ) => {
   if (maze == "BINARY_TREE") {
     await binaryTree(grid, startTile, endTile, setIsDisabled, speed);
+  } else if (maze == "RECURSIVE_DIVISION") {
+    const currentSpeed = SPEEDS.find((s) => s.value === speed)!.value ?? 2;
+    await constructBoarder(grid, startTile, endTile);
+    await recursiveDivision({
+      grid,
+      startTile,
+      endTile,
+      row: 1,
+      col: 1,
+      height: Math.floor(MAX_ROWS - 1) / 2,
+      width: Math.floor(MAX_COLS - 1) / 2,
+      setIsDisabled,
+      speed,
+    });
+    setTimeout(() => {
+      setIsDisabled(false);
+    }, 800 * currentSpeed);
   }
 };
