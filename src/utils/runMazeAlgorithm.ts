3ce1aa542a50520a417fc06fb38af104927253f4
folka2134 <folka2134@gmail.com>
2024-07-25 16:49:01 +0100

    fixed import bug

--- src/utils/runMazeAlgorithm.ts
+++ src/utils/runMazeAlgorithm.ts
@@ -1,29 +1,27 @@
 import { binaryTree } from "../lib/algorithms/maze/binaryTree";
-import { recursiveDivision } from "../lib/algorithms/maze/recursiveDivision";
+import recursiveDivision from "../lib/algorithms/maze/recursiveDivision";
 import { MAX_COLS, MAX_ROWS, SPEEDS } from "./constants";
 import { constructBoarder } from "./constructBoarder";
 import { GridType, MazeType, SpeedType, TileType } from "./types";
 
-// type runMazeALgorithmProps = {
-//   maze: MazeType;
-//   grid: GridType;
-//   startTile: TileType;
-//   endTile: TileType;
-//   setIsDisabled: (issDisabled: boolean) => void;
-//   speed: SpeedType;
-// };
-
-export const runMazeALgorithm = async (
-  maze: MazeType,
-  grid: GridType,
-  startTile: TileType,
-  endTile: TileType,
-  setIsDisabled: (issDisabled: boolean) => void,
-  speed: SpeedType,
-) => {
+export const runMazeAlgorithm = async ({
+  maze,
+  grid,
+  startTile,
+  endTile,
+  setIsDisabled,
+  speed,
+}: {
+  maze: MazeType;
+  grid: GridType;
+  startTile: TileType;
+  endTile: TileType;
+  setIsDisabled: (isDisabled: boolean) => void;
+  speed: SpeedType;
+}) => {
   if (maze == "BINARY_TREE") {
     await binaryTree(grid, startTile, endTile, setIsDisabled, speed);
-  } else if (maze == "RECURSIVE_DIVISION") {
+  } else if (maze === "RECURSIVE_DIVISION") {
     const currentSpeed = SPEEDS.find((s) => s.value === speed)!.value ?? 2;
     await constructBoarder(grid, startTile, endTile);
     await recursiveDivision({
@@ -32,8 +30,8 @@ export const runMazeALgorithm = async (
       endTile,
       row: 1,
       col: 1,
-      height: Math.floor(MAX_ROWS - 1) / 2,
-      width: Math.floor(MAX_COLS - 1) / 2,
+      height: Math.floor((MAX_ROWS - 1) / 2),
+      width: Math.floor((MAX_COLS - 1) / 2),
       setIsDisabled,
       speed,
     });
