19e82de1436b709cfbe7dccf4484ce13bb1d9d4a
folka2134 <folka2134@gmail.com>
2024-07-28 18:03:54 +0100

    added sorting algorithm types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -30,8 +30,30 @@ export interface SpeedSelectType {
   value: SpeedType;
 }
 
-export type SortingAlgorithmType = "BUBBLE" | "QUICK" | "MERGE";
-export interface SortingAlgorithmTypeSelectType {
+export type SortingAlgorithmType =
+  | "bubble"
+  | "insertion"
+  | "selection"
+  | "merge"
+  | "quick";
+
+export type AlgorithmInfo = {
+  title: string;
+  description: string;
+  worstCase: string;
+  averageCase: string;
+  bestCase: string;
+};
+
+export type SortingAlgorithmsData = {
+  [key in SortingAlgorithmType]: AlgorithmInfo;
+};
+
+export type AnimateSpeedType = "slow" | "medium" | "fast" | "lighning";
+
+export type SelectOptionsType = {
   name: string;
-  value: SortingAlgorithmType;
-}
+  value: string;
+};
+
+export type AnimationArrayType = [number[], boolean][];
