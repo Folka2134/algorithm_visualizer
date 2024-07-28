5dbdd8f95821116d1ca7a9f168d1aaafa0c67710
folka2134 <folka2134@gmail.com>
2024-07-28 16:07:31 +0100

    added home button

--- src/components/PathOptions.tsx
+++ src/components/PathOptions.tsx
@@ -16,6 +16,8 @@ import { runMazeAlgorithm } from "../utils/runMazeAlgorithm";
 import { runPathFindingAlgorithm } from "../utils/runPathFindingAlgorithm";
 import { animatePath } from "../utils/animatePath";
 import { PlayButton } from "./PlayButton ";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 const PathOptions = ({
   isVisualizationRunningRef,
@@ -93,6 +95,11 @@ const PathOptions = ({
   return (
     <div className="flex min-h-[4.5rem] items-center justify-center border-b px-0 shadow-gray-600 sm:px-5">
       <div className="flex w-full items-center justify-center sm:w-[52rem] lg:justify-between">
+        <button className="mr-8 text-white lg:mr-4">
+          <Link to="/">
+            <FaArrowLeft size={24} />
+          </Link>
+        </button>
         <h1 className="hidden w-[40%] pl-1 text-3xl text-white lg:flex">
           Pathfinder
         </h1>
