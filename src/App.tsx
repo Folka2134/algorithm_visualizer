3e1beb956bd8b6d02265d9962d94bb0d67a2d83d
folka2134 <folka2134@gmail.com>
2024-07-30 13:51:39 +0100

    added project previews

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,35 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex h-screen w-screen flex-col justify-center gap-5 bg-[radial-gradient(#ffffff33_1px,#121212_1px)] bg-[size:40px_40px] text-white lg:flex-row">
-      <button>
-        <Link to="/pather">Path Finder</Link>
-      </button>
-      <button>
-        <Link to="/sorter">Bar sorter</Link>
-      </button>
+    <div className="flex min-h-screen w-screen flex-col justify-center gap-5 bg-[radial-gradient(#ffffff33_1px,#121212_1px)] bg-[size:40px_40px] text-white lg:flex-row">
+      <div className="mt-5 flex items-center justify-center md:mt-0">
+        <Link to="/pather">
+          <div className="group">
+            <h3 className="mb-3 text-center text-2xl font-semibold">
+              Path Finder
+            </h3>
+            <img
+              src="src/assets/pathfinder_preview.png"
+              alt="path finder preview"
+              className="transition-transform duration-100 group-hover:scale-105"
+            />
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center justify-center">
+        <Link to="/sorter">
+          <div className="group">
+            <h3 className="mb-3 text-center text-2xl font-semibold">
+              Bar sorter
+            </h3>
+            <img
+              src="src/assets/sorter_preview.png"
+              alt="path finder preview"
+              className="transition-transform duration-100 group-hover:scale-105"
+            />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
